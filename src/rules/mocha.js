e74293025cc0dbb558303d3263770497ee3e6296
Renke Grunwald <renke@renke.org>
2016-02-21 13:29:01 +0100

    Fix mocha rules

--- src/rules/mocha.js
+++ src/rules/mocha.js
@@ -1,10 +1,10 @@
 export default {
   // no-exclusive-tests - disallow exclusive mocha tests (fixable)
-  "no-exclusive-tests": [1],
+  "mocha/no-exclusive-tests": [1],
   // handle-done-callback - enforces handling of callbacks for async tests
-  "handle-done-callback": [2],
+  "mocha/handle-done-callback": [2],
   // no-synchronous-tests - disallow synchronous tests
-  "no-synchronous-tests": [2],
+  "mocha/no-synchronous-tests": [2],
   // no-global-tests - disallow global tests
-  "no-global-tests": [2],
+  "mocha/no-global-tests": [2],
 };
